393a6b4ff4f07c4796d1fd22536b099581c6f955
Arfaz <arfazchhapawala.otobit@gmail.com>
2022-05-12 15:41:05 +0530

    Authentication page first modification

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Foodapp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: const OnboardView(),
+      home: const AuthenticationView(),
     );
   }
 }
